e092badfb6dc7bb0fc44385c808c2a88b33114dd
Franklin Shera <fshera96@gmail.com>
2022-02-18 00:32:35 +0300

    Navigation

---
+++ StackNavigator.js
@@ -0,0 +1,27 @@
+import { View, Text } from "react-native";
+import React from "react";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import HomeScreen from "./screens/HomeScreen";
+import ChatScreen from "./screens/ChatScreen";
+import LoginScreen from "./screens/LoginScreen";
+
+const StackNavigator = () => {
+  const Stack = createNativeStackNavigator();
+
+  const auth = true;
+
+  return (
+    <Stack.Navigator>
+      {auth ? (
+        <>
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="Chat" component={ChatScreen} />
+        </>
+      ) : (
+        <Stack.Screen name="Login" component={LoginScreen} />
+      )}
+    </Stack.Navigator>
+  );
+};
+
+export default StackNavigator;
