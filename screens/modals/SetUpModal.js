d33b57ec3fe4214bdbcf03ce55c3b1c2ee839966
Franklin Shera <fshera96@gmail.com>
2022-02-22 06:26:29 +0300

    Keaboard Avoiding & Dismiss

--- screens/modals/SetUpModal.js
+++ screens/modals/SetUpModal.js
@@ -7,8 +7,11 @@ import {
   StatusBar,
   Image,
   TouchableOpacity,
+  KeyboardAvoidingView,
+  TouchableWithoutFeedback,
+  Keyboard,
 } from "react-native";
-import React from "react";
+import React, { useState } from "react";
 
 import getState from "../../hooks/appState";
 import InputField from "../../components/form-inputs/InputField";
@@ -16,33 +19,71 @@ import InputField from "../../components/form-inputs/InputField";
 const SetUpModal = () => {
   // @ts-ignore
   const { user } = getState();
+
+  const [imageUrl, setImageUrl] = useState("");
+  const [job, setJob] = useState("");
+  const [age, setAge] = useState("");
+
+  const formIncomplete = !imageUrl || !job || !age;
+
   return (
     <SafeAreaView style={styles.main}>
-      <View style={styles.header}>
-        <Image
-          style={styles.logo}
-          resizeMode="contain"
-          source={{ uri: "https://links.papareact.com/2pf" }}
-        />
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        style={{ flex: 1 }}
+      >
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+          <View style={styles.header}>
+            <Image
+              style={styles.logo}
+              resizeMode="contain"
+              source={{ uri: "https://links.papareact.com/2pf" }}
+            />
 
-        <Text style={styles.welcomeTxt}>Welcome {user.displayName}</Text>
+            <Text style={styles.welcomeTxt}>Welcome {user.displayName}</Text>
 
-        <InputField
-          label="Stap 1 : Profile Picture"
-          placeholder="Enter Profile Picture URL"
-        />
+            <InputField
+              label="Stap 1 : Profile Picture"
+              placeholder="Enter profile picture url"
+              inputValue={imageUrl}
+              onChange={(text) => setImageUrl(text)}
+            />
 
-        <InputField
-          label="Stap 2 : Occupation"
-          placeholder="Enter Your Occupation"
-        />
+            <InputField
+              label="Stap 2 : Occupation"
+              placeholder="Enter your occupation"
+              inputValue={job}
+              onChange={(text) => setJob(text)}
+            />
 
-        <InputField label="Stap 3 : Age" placeholder="Enter Your Age" />
+            <InputField
+              label="Stap 3 : Age"
+              placeholder="Enter your age"
+              inputValue={age}
+              onChange={(text) => setAge(text)}
+              // @ts-ignore
+              keyboardType="numeric"
+              maxLength={2}
+            />
 
-        <TouchableOpacity style={styles.updateBtn}>
-          <Text style={styles.updateBtnTxt}>Update Profile</Text>
-        </TouchableOpacity>
-      </View>
+            <TouchableOpacity
+              disabled={formIncomplete}
+              style={[
+                styles.updateBtn,
+                formIncomplete
+                  ? {
+                      backgroundColor: "#9fa3ae",
+                    }
+                  : {
+                      backgroundColor: "#ff5865",
+                    },
+              ]}
+            >
+              <Text style={styles.updateBtnTxt}>Update Profile</Text>
+            </TouchableOpacity>
+          </View>
+        </TouchableWithoutFeedback>
+      </KeyboardAvoidingView>
     </SafeAreaView>
   );
 };
@@ -61,7 +102,6 @@ const styles = StyleSheet.create({
   },
   updateBtn: {
     width: 200,
-    backgroundColor: "#ff5865",
     padding: 15,
     position: "absolute",
     bottom: 40,
