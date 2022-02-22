299d41d1db03f92818b4465fe570d0931588f87d
Franklin Shera <fshera96@gmail.com>
2022-02-22 07:11:26 +0300

    Loader Updated

--- components/Loader.js
+++ components/Loader.js
@@ -1,9 +1,12 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
+import { Fontisto, Ionicons, Entypo } from "@expo/vector-icons";
 
 const Loader = () => {
   return (
     <View style={styles.parent}>
+      <Fontisto name="tinder" size={86} color="#ff5865" />
+
       <Text style={styles.txt}>Loading...</Text>
     </View>
   );
@@ -14,11 +17,13 @@ export default Loader;
 const styles = StyleSheet.create({
   parent: {
     display: "flex",
-    padding: 50,
-    backgroundColor: "#9a444e",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "white",
+    flex: 1,
   },
   txt: {
-    color: "white",
-    fontSize: 32,
+    color: "#ff5865",
+    fontSize: 22,
   },
 });
